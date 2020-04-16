e08afa69df1e4021f354b06ad7adb43a18137f28
Juan J Chamie <56560982+JQ-dev@users.noreply.github.com>
2020-04-16 12:06:52 -0400

    Update gestionCOVID.js

--- WDC_COVID-19/js/gestionCOVID.js
+++ WDC_COVID-19/js/gestionCOVID.js
@@ -1,1 +1,22 @@
+(function(){
+	var _wdc = new WDC();
+	_wdc.ConsultarDatos();
+	if(_wdc.getRespuesta().valido && _wdc.getRespuesta().respuesta){
+		var connector = tableau.makeConnector();
+		connector.getSchema = function(schemaCallback) {
+			var table = _wdc.CrearEsquemaCOVID(tableau.dataTypeEnum);
+			schemaCallback([table]);
+		};
 
+		connector.getData = function(table, doneCallback){		
+			table.appendRows(_wdc.CrearDatosCOVID());
+			doneCallback();
+		};
+
+		tableau.registerConnector(connector);
+		connector.init = function(initCallback) {
+			initCallback();
+			tableau.submit();
+		};
+	}
+})();
